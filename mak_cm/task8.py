7adccf28c31c424aaf5235f7a92dc55c5d14bed6
Makukha_Ilia <makukha.it@gmail.com>
2021-08-23 16:09:56 +0300

    task8 was ended

--- mak_cm/task8.py
+++ mak_cm/task8.py
@@ -4,7 +4,9 @@
 import numpy as np
 from scipy.integrate import simps
 from scipy.special import eval_jacobi, jacobi
+from tqdm import tqdm
 import matplotlib.pyplot as plt
+import matplotlib.animation as ani
 
 from task7 import BoundaryProblem, solve_bp_system
 
@@ -58,6 +60,7 @@ def integration_wrapper(int_fun):
 class RitzSolver():
     '''Алгоритм Ритца'''
     def __init__(self, rfp, basis, integrator='scipy', n=100):
+        '''n - число делений для интегрирования'''
         self.rfp = rfp
         self.basis = basis
         self.set_integrator(integrator)
@@ -138,17 +141,86 @@ def jacobi_dx(n):
     return lambda x: np.sum(coefs_dx * np.power(x, np.arange(n + 1)))
 
 
-def get_jacobi_basis(n):
-    '''Возращает базис'''
-    return Basis([jacobi_polynom(i) for i in range(n)],
-                 [jacobi_dx(i) for i in range(n)])
+def get_basis_1(n):
+    '''Возращает базис для первого варианта'''
+    def foo(poly):
+        def bar(x):
+            return (1-x**2)*poly(x)
+        return bar
+    
+    def foo2(poly, poly_diff):
+        def bar2(x):
+            return (1-x**2)*poly_diff(x) - 2*x*poly(x)
+        return bar2
+
+    l1 = list()
+    l2 = list()
+    for i in range(n):
+        poly = jacobi_polynom(i)
+        poly_diff = jacobi_dx(i)
+        l1.append(foo(poly))
+        l2.append(foo2(poly, poly_diff))
+    return Basis(l1, l2)
+
+
+def plot_results(bp, grid_n, basis_generator, max_basis_n, rfp,
+                 int_n, integrator, xlim, ylim, name):
+    '''Анимированные результаты
+    
+    Parameters
+    ----------
+    bp : BoundaryProblem
+        Задача для метода сетки
+    grid_n : int
+        Число деления для метода сетки
+    basis_generator : function(i)
+        Функция, генерирующая базис по заданному i
+    max_basis_n : int 
+        максимальный размер базиса
+    rfp : RitzFormProblem
+        Задача для метода Ritz
+    int_n : int
+        Число делений для интегрирования
+    integrator : str
+        Вид интегратора (scipy, numpy, self-made)
+    xlim и ylim: 
+        Границы графика
+    name: str 
+        '<название_анимации>.html'
+    '''
+    # Вычисление методом сетки
+    u = solve_bp_system(bp, grid_n)
+    u_len = len(u)
+    x = np.linspace(-1, 1, u_len)
+
+    # Вычисление методом Ритца
+    u_l = list()
+    for i in tqdm(range(1, max_basis_n)):
+        basis = basis_generator(i)
+        solver = RitzSolver(rfp, basis, integrator, int_n)
+        coefs = solver.solve()
+        u2 = np.vectorize(lambda x: comp(x, basis, coefs)) (x)
+        u_l.append(u2)
+
+    # Анимация
+    fig, ax = plt.subplots()
+    m = len(u_l)
+
+    def animate(i):
+        ax.clear()
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+        u2 = u_l[i]
+        ax.plot(x, u2)
+        ax.plot(x, u)
+        ax.set_title(f'i={i + 1}')
+    
+    animation = ani.FuncAnimation(fig, animate, frames=m, interval=200, repeat=False)
+    animation.save(name, writer='html', fps=1)
 
 
 if __name__ == '__main__':
     np.random.seed(111)
-    grid_n = 10**3
-    basis_n = 30
-
     bp = BoundaryProblem(
         k=lambda x: - 1 / (x + 2),
         p=lambda x: 1 / (x + 2)**2,
@@ -163,11 +235,6 @@ if __name__ == '__main__':
         a=-1,
         b=1
     )
-    u = solve_bp_system(bp, grid_n)
-
-    u_len = len(u)
-    x = np.linspace(-1, 1, u_len)
-
     rfp = RitzFormProblem(
         p=lambda x: 1 / (2 + x),
         r=np.cos,
@@ -177,11 +244,15 @@ if __name__ == '__main__':
         beta1=1,
         beta2=0
     )
-    basis = get_jacobi_basis(basis_n)
-    solver = RitzSolver(rfp, basis)
-    coefs = solver.solve()
-    u2 = np.vectorize(lambda x: comp(x, basis, coefs)) (x)
-    
-    plt.plot(x, u)
-    plt.plot(x, u2)
-    plt.show()
+    plot_results(
+        bp=bp,
+        grid_n=1000,
+        basis_generator=get_basis_1,
+        max_basis_n=60,
+        rfp=rfp,
+        integrator='scipy',
+        int_n=100,
+        xlim=(-1, 1),
+        ylim=(-1, 3),
+        name='ritz_1_animation.html'
+    )
